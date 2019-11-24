5c82fdfef0d31a4f9c3743fb150074d42ba8800d
Stef997 <stefan.cross@ontariotechu.net>
2019-11-24 07:47:01 -0500

    Added Interactions for the elements and ConfirmActivity activated by the confirm button in GroupActivity

--- app/src/main/java/com/example/timehunter/MainActivity.kt
+++ app/src/main/java/com/example/timehunter/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.timehunter
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.ImageButton
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.provider.ContactsContract
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val createGroup = Intent(this@MainActivity, GroupActivity::class.java)
+        startActivity(createGroup)
     }
 }
+

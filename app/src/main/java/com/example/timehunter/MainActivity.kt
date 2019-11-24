b147926fee8288df83994f3d8a431a96c41a10a2
msetnyk <msetnyk@gmail.com>
2019-11-23 22:10:36 -0500

    nav to home from drawer

--- app/src/main/java/com/example/timehunter/MainActivity.kt
+++ app/src/main/java/com/example/timehunter/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.timehunter
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -24,5 +27,17 @@ class MainActivity :  AppCompatActivity() {
         val navView = findViewById<NavigationView>(R.id.nav_view)
         navView.setupWithNavController(navController)
         toolbar.title=""
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.navigation, menu);
+
+        return true
     }
 }

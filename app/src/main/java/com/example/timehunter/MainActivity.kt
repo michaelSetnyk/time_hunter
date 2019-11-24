15d0ca4fb936f410f53ebe4d5c676b7dc3e9b94f
msetnyk <msetnyk@gmail.com>
2019-11-23 20:49:29 -0500

    nav drawer and toolbar

--- app/src/main/java/com/example/timehunter/MainActivity.kt
+++ app/src/main/java/com/example/timehunter/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.timehunter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity :  AppCompatActivity() {
+    private lateinit var appBarConfig: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val toolbar =  findViewById<Toolbar>(R.id.toolbar)
+        appBarConfig = AppBarConfiguration(navController.graph, drawer_layout)
+
+        toolbar.setupWithNavController(navController,appBarConfig)
+
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+        navView.setupWithNavController(navController)
+        toolbar.title=""
     }
 }

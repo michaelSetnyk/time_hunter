7922ae03c07c937cf9318b12527ad9548289682b
msetnyk <msetnyk@gmail.com>
2019-11-24 20:27:32 -0500

    ugly group page with nav from groups page

--- app/src/main/java/com/example/timehunter/Group.kt
+++ app/src/main/java/com/example/timehunter/Group.kt
@@ -5,9 +5,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 
-class Group (val name: String, val summary: String, val icon :Int)
+class Group (val name: String, val summary: String, val icon :Int, var people: ArrayList<User> = ArrayList<User>())
 
 class GroupAdapter (private val groups:ArrayList<Group>):
     RecyclerView.Adapter<GroupAdapter.GroupHolder>() {
@@ -16,11 +21,15 @@ class GroupAdapter (private val groups:ArrayList<Group>):
         val nameView: TextView
         val imageView: ImageView
         val summaryView : TextView
+        val cardView: CardView
+        var manager: FragmentManager
 
         init {
             nameView = v.findViewById(R.id.name)
             summaryView = v.findViewById(R.id.summary)
             imageView = v.findViewById(R.id.photo)
+            cardView = v.findViewById(R.id.group_item)
+            manager = (v.context as FragmentActivity).supportFragmentManager
         }
     }
 
@@ -30,15 +39,16 @@ class GroupAdapter (private val groups:ArrayList<Group>):
     ): GroupHolder {
         val layout =
             LayoutInflater.from(parent.context).inflate(R.layout.group_item, parent, false)
-
         return GroupHolder(layout)
     }
+
 
     override fun onBindViewHolder(holder: GroupHolder, position: Int) {
         val group = groups[position]
         holder.imageView.setImageResource(group.icon)
         holder.nameView.text = group.name
-        holder.summaryView.text = group.summary
+        val a = ViewGroupFragment.newInstance(group).arguments
+        holder.cardView.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.viewGroupFragment,a))
     }
 
     override fun getItemCount() = groups.size
